37ed71c65c2013ce58761417410545789178f84a
Jones Agyemang <mightyj@hotmail.co.uk>
2025-05-22 10:57:59 +0000

    JA - Configure to use Redis instance

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,17 +2,17 @@ require "sidekiq"
 require "sidekiq-cron"
 
 Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch("REDIS_URL") }
   config.logger.level = Logger::DEBUG
 end
 
 Sidekiq.configure_server do |config|
-  # configure scheduler
+  config.redis = { url: ENV.fetch("REDIS_URL") }
   schedule_file = Rails.root.join("config/sidekiq.yml")
 
   if File.exist?(schedule_file)
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
   end
 
-  # enable verbose logging
   config.logger.level = Logger::DEBUG
 end
