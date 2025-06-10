d19c8f1a6fd938f8a54c935f2dac6c1b9941d43b
Jones Agyemang <mightyj@hotmail.co.uk>
2025-06-10 05:11:25 +0100

    JA - Set default REDIS_URL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,12 +2,12 @@ require "sidekiq"
 require "sidekiq-cron"
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") }
+  config.redis = { url: ENV.fetch("REDIS_URL", "") }
   config.logger.level = Logger::DEBUG
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") }
+  config.redis = { url: ENV.fetch("REDIS_URL", "") }
   schedule_file = Rails.root.join("config/sidekiq.yml")
 
   if File.exist?(schedule_file)
