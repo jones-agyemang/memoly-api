0b7432a65b45f64fb57782d54313e2ddff49b6e7
Jones Agyemang <mightyj@hotmail.co.uk>
2025-06-12 06:34:09 +0100

    JA - Specifiy user email

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -2,11 +2,12 @@ class ReminderMailer < ApplicationMailer
   default from: "notification@memoly.io"
 
   def due_notes_email
+    user = User.find params[:user_id]
     @contents = params[:notes]
 
     mail(
       subject: "Notes for Today",
-      to: "test@blackhole.postmarkapp.com",
+      to: user&.email,
       track_opens: true,
       message_stream: "outbound"
     )
