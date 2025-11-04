8d681bc874f62e209cb7329590a1533c723f8096
Jones Agyemang <mightyj@hotmail.co.uk>
2025-11-04 19:50:16 +0000

    Link Sprockets to directories only

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,5 @@
-//= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-//= link_directory ../fonts/katex .ttf
-//= link_directory ../fonts/katex .woff
-//= link_directory ../fonts/katex .woff2
-//= require katex
+//= link_directory ../fonts .ttf
+//= link_directory ../fonts .woff
+//= link_directory ../fonts .woff2
