6f535f335e053da3a01875e1c767cf47324fb9c8
Jones Agyemang <mightyj@hotmail.co.uk>
2025-11-07 14:53:33 +0000

    Correct method name

--- db/data/20251107142522_assign_uncategorised_notes_to_default_collection.rb
+++ db/data/20251107142522_assign_uncategorised_notes_to_default_collection.rb
@@ -4,7 +4,7 @@ class AssignUncategorisedNotesToDefaultCollection < ActiveRecord::Migration[8.0]
   def up
     default_collection = Collection.find_by(label: Collection::DEFAULT_CATEGORY_LABEL)
 
-    Note.for_each do |note|
+    Note.find_each do |note|
       next if note.collection.present?
 
       note.collection = default_collection
