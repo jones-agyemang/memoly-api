f8ab9d6004c28596cc2dbf70df92152714e2f4f5
Jones Agyemang <mightyj@hotmail.co.uk>
2025-11-07 14:31:18 +0000

    Assigns uncategorised notes to default notes

---
+++ db/data/20251107142522_assign_uncategorised_notes_to_default_collection.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class AssignUncategorisedNotesToDefaultCollection < ActiveRecord::Migration[8.0]
+  def up
+    default_collection = Collection.find_by(label: Collection::DEFAULT_CATEGORY_LABEL)
+
+    Notes.for_each do |note|
+      next if note.collection.present?
+
+      note.collection = default_collection
+      note.save
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
