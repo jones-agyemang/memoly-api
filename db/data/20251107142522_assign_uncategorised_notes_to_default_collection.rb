618c78f9205ee7ed9feb7397181119a098fd80f4
Jones Agyemang <mightyj@hotmail.co.uk>
2025-11-07 14:50:16 +0000

    Fix name error for Note model

--- db/data/20251107142522_assign_uncategorised_notes_to_default_collection.rb
+++ db/data/20251107142522_assign_uncategorised_notes_to_default_collection.rb
@@ -4,7 +4,7 @@ class AssignUncategorisedNotesToDefaultCollection < ActiveRecord::Migration[8.0]
   def up
     default_collection = Collection.find_by(label: Collection::DEFAULT_CATEGORY_LABEL)
 
-    Notes.for_each do |note|
+    Note.for_each do |note|
       next if note.collection.present?
 
       note.collection = default_collection
