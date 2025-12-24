2f4740c0a83987d574d63ad17b1ddd834dc19e1e
Jones Agyemang <mightyj@hotmail.co.uk>
2025-12-24 13:09:29 +0000

    Add empty config

--- spec/cron_schedule_spec.rb
+++ spec/cron_schedule_spec.rb
@@ -2,8 +2,16 @@ require "rails_helper"
 
 RSpec.describe "Cron scheduler" do
   let(:schedule_loader) { Sidekiq::Cron::ScheduleLoader.new }
+  let(:schedule_path) { Rails.root.join("config/schedule.yml") }
 
   it "has a schedule file" do
     expect(schedule_loader).to have_schedule_file
+  end
+
+  it "does not schedule reminders when disabled" do
+    schedule = YAML.load_file(schedule_path)
+
+    expect(schedule).to be_a(Hash)
+    expect(schedule).not_to include("send_due_reminders")
   end
 end
