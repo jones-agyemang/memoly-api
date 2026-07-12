0df3bd5a14785f6c36a945f9a80d0375c091db08
Jones Agyemang <mightyj@hotmail.co.uk>
2026-07-12 08:45:53 +0100

    Orchestrate parsing and consumption

--- app/workers/source_parser_worker.rb
+++ app/workers/source_parser_worker.rb
@@ -4,6 +4,7 @@ class SourceParserWorker
   include Sidekiq::Job
 
   def perform(source_intake_id)
-    SourceParser.call(source_intake_id:)
+    parsed_source = SourceParser.call(source_intake_id:)
+    SourceConsumer.call(SourceIntake.find(source_intake_id), parsed_source)
   end
 end
--- spec/workers/source_parser_worker_spec.rb
+++ spec/workers/source_parser_worker_spec.rb
@@ -2,11 +2,37 @@ require "rails_helper"
 
 RSpec.describe SourceParserWorker, type: :worker do
   describe '#perform' do
-    it 'delegates parsing to SourceParser with source intake id' do
-      source_intake_id = 1
-      expect(SourceParser).to receive(:call).with(source_intake_id:)
+    context "when source is parseable" do
+      it "parses the source and consumes the parsed source" do
+        arguments = {
+          collections: {
+            "Sidekiq API" => {
+              parent_label: nil,
+              position: 0,
+              notes: [ "# Intro" ]
+            }
+          }
+        }
+        source_intake =  create(:source_intake)
+        allow(SourceParser).to receive(:call).with(source_intake_id: source_intake.id).and_return(arguments)
+        expect(SourceConsumer).to receive(:call).with(source_intake, arguments)
 
-      described_class.new.perform(source_intake_id)
+        described_class.new.perform(source_intake.id)
+      end
+    end
+
+    context "when source is unparseable" do
+      it "does not call the consumer when parsing fails" do
+        source_intake = create(:source_intake)
+
+        allow(SourceParser).to receive(:call).with(source_intake_id: source_intake.id).and_raise(JSON::ParserError)
+
+        expect(SourceConsumer).not_to receive(:call)
+
+        expect do
+          described_class.new.perform(source_intake.id)
+        end.to raise_error(JSON::ParserError)
+      end
     end
   end
 end
