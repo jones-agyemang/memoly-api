5e97c211e71c37a19a8be176941f1e9357c0a6f4
Jones Agyemang <mightyj@hotmail.co.uk>
2026-08-13 19:28:30 +0100

    Update authentication mailer view

--- spec/mailers/previews/send_requested_code_mailer_preview.rb
+++ spec/mailers/previews/send_requested_code_mailer_preview.rb
@@ -2,6 +2,9 @@
 class SendRequestedCodeMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/send_requested_code_mailer_mailer/send_authentication_code
   def send_authentication_code
-    SendRequestedCodeMailer.send_authentication_code
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:authentication_code, user:)
+
+    SendRequestedCodeMailer.with(user_id: user.id).send_authentication_code
   end
 end
